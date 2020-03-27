81eaf6a9965412a022add8a2f53422d3e35c9652
Rafael Ferreira Umbelino <umbelino12897@gmail.com>
2020-03-27 16:53:00 -0300

    teste cors

--- src/app.js
+++ src/app.js
@@ -6,6 +6,31 @@ const { corsConfig } = require("./settings");
 const app = express();
 
 app.use(cors(corsConfig));
+app.use(
+  app.use(function(req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:4400");
+
+    // Request methods you wish to allow
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+    );
+
+    // Request headers you wish to allow
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "X-Requested-With,content-type"
+    );
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader("Access-Control-Allow-Credentials", true);
+
+    // Pass to next layer of middleware
+    next();
+  })
+);
 app.use(express.json());
 addRoutes(app);
 
--- src/settings.js
+++ src/settings.js
@@ -8,7 +8,6 @@ module.exports = {
       "X-Access-Token"
     ],
     credentials: true,
-    "Access-Control-Allow-Origin": "http://localhost:4400",
     methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
     origin: "http://localhost:4400",
     preflightContinue: false
